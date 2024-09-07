eb114fed97922bde4639fe74ba0e2e93d074a38f
Ibrahim-Sahabi <isahabi@yahoo.com>
2024-09-06 21:16:07 -0400

    module files reconfigured

---
+++ eks/backend.tf
@@ -0,0 +1,22 @@
+terraform {
+  #required_version = "~> 1.9.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+}
+
+  terraform { 
+    backend "s3" {
+    bucket         = "teamusa-test-bucket"
+    region         = "us-east-2"
+    key            = "eks1/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+}
+provider "aws" {
+  region  = var.aws-region
+}
